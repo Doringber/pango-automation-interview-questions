03507d1296321d09447b41ac4500c3ec16a7abcf
Noam17135 <nmr17135@gmail.com>
2025-06-17 02:36:45 +0300

    revert the API details from .env beacuse it ignorded in the gitignored

--- automation_framework/utilities/api_helpers.py
+++ automation_framework/utilities/api_helpers.py
@@ -1,17 +1,13 @@
 import requests
 import os
-from dotenv import load_dotenv
-load_dotenv('config/.env')
 
 class ApiHelper:
-    BASE_URL = os.getenv("API_BASE_URL")
-    API_KEY = os.getenv("API_KEY")
-
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+    API_KEY = "f88d54cd8eddb5d1f23ff82a80b95fec"
+    
     def get_current_weather(self, city):
         url = f"{self.BASE_URL}?q={city}&appid={self.API_KEY}"
         print(url)
         response = requests.get(url)
         print(response.json())
         return response
-
-
